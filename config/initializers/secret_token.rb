2830ea588bee7467431d9f04676db6352e4012e4
Flarnie Marchan <fnonemaker@gmail.com>
2014-01-27 16:09:59 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marshmellow::Application.config.secret_token = 'eba77c7bd51e81db55b8f04fabbeeff872d07c16bb5201b7629095ded3d83f0a9c0d504dd4d8093873c4ec928ff4f3341d66aa94c3041a578a4f8c70b7a655bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Marshmellow::Application.config.session_store :cookie_store, key: '_marshmellow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Marshmellow::Application.config.session_store :active_record_store
