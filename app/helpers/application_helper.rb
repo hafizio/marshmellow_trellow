b9be04defadb1e0cdc48476519b8c9bd86872526
Flarnie Marchan <fnonemaker@gmail.com>
2014-01-29 10:03:59 -0800

    Add user gravatar and username to user, navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
 end
