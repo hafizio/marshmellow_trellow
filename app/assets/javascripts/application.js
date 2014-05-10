96fd5f677f893a09a179f070f2d14bb3a8cb4c1a
Flarnie Marchan <fnonemaker@gmail.com>
2014-05-10 07:56:31 -0700

    happy cupcake-ify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require happy_cupcake
 //= require underscore
 //= require backbone
 //= require react
---
+++ vendor/assets/javascripts/happy_cupcake.js
@@ -0,0 +1,2 @@
+// Happy Cupcake Javascript Manifest
+//= require_tree ./happy_cupcake
