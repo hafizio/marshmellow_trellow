ceba24492d4e81e44eb2e42c6046ac4a3aac9dff
Flarnie Marchan <fnonemaker@gmail.com>
2014-05-11 06:09:42 -0700

    Add reusable show/hide react component

---
+++ app/assets/javascripts/reactComponents/showHide.js.jsx
@@ -0,0 +1,26 @@
+Marshmellow.showHide = React.createClass({
+  getInitialState: function() {
+    return { hidden: true };
+  },
+
+  toggleContent: function() {
+    this.setState({ hidden: !this.state.hidden });
+  },
+
+  render: function() {
+    // We could use JS to manually set the display:none property
+    // But it's better to toggle a CSS class when you can
+    var hideClass = (this.state.hidden) ? "hide" : "";
+    return (
+      <div className={"show-hide " + this.props.additionalClasses} >
+        <a className={"show-hide__toggle"} onClick={this.toggleContent} >
+          {this.props.toggleText}
+        </a>
+        <div className={"show-hide__content " + hideClass} ref="content">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+
+});
